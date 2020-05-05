96abb63fa6fa6c70f899671374b1d0358ebc2f24
Lilian Saget-Lethias <lilian.sagetlethias@gmail.com>
2020-05-05 02:12:24 +0200

    :sparkles: Add debug and shorten channel param

--- api/youtube-chat.ts
+++ api/youtube-chat.ts
@@ -1,55 +1,72 @@
 // Inspired from https://github.com/bigmonmulgrew/YouTube-Live-Chat-URL-finder
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'https://deno.land/x/lambda/mod.ts';
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Context,
+} from "https://deno.land/x/lambda/mod.ts";
 
 const YOUTUBE_BASE_URL = "https://youtube.com";
+const CHANNEL_URL = (channelId: string) => `${YOUTUBE_BASE_URL}/channel/${channelId}/live`
+const LIVE_URL = (liveId: string) => `${YOUTUBE_BASE_URL}/live_chat?v=${liveId}&is_popout=1`;
 
-export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
-    const channelUrl = event.queryStringParameters?.channel;
+/**
+ * http://localhost:3000/api/youtube-chat?channel=UCbwGFj1MoXSko_QgutV4_Cg
+ */
+export async function handler(
+  event: APIGatewayProxyEvent,
+  context: Context,
+): Promise<APIGatewayProxyResult> {
+  const body: { path: string } = JSON.parse(event.body ?? "{}");
+  const params = new URLSearchParams(body.path.split("?")[1]);
+  const channelId = params.get("channel") ?? "";
 
-    try {
-        validYouTubeChannel(channelUrl);
-        const liveId = await findLiveId(channelUrl);
-        const liveUrl = `${YOUTUBE_BASE_URL}/live_chat?v=${liveId}&is_popout=1`
+  try {
+    const liveId = await findLiveId(CHANNEL_URL(channelId));
+    const liveUrl = LIVE_URL(liveId);
 
-        return {
-            body: `Redirecting to ${liveUrl}`,
-            headers: {
-                'content-type': 'text/html; charset=utf-8',
-                'location': liveUrl
-            },
-            statusCode: 301,
-        };
-    } catch (e) {
-        return {
-            body: e.message,
-            headers: {
-                'content-type': 'text/html; charset=utf-8',
-            },
-            statusCode: 400,
-        }
+    if (params.has('debug')) {
+      return {
+        body: `Redirecting to ${liveUrl}`,
+        headers: {
+          "content-type": "text/html; charset=utf-8",
+        },
+        statusCode: 200,
+      };
     }
+
+    return {
+      body: ``,
+      headers: {
+        'location': liveUrl
+      },
+      statusCode: 301,
+    };
+  } catch (e) {
+    return {
+      body: e.message,
+      headers: {
+        "content-type": "text/html; charset=utf-8",
+      },
+      statusCode: 400,
+    };
+  }
 }
-
-function validYouTubeChannel(channelUrl?: string): asserts channelUrl {
-    if (!channelUrl || channelUrl.startsWith(YOUTUBE_BASE_URL)) {
-        throw new Error('Invalid YouTube channel URL.');
-    }
-
-    if (!/\/live$/.test(channelUrl)) {
-        throw new Error('YouTube channel URL should end with "/live". e.g "https://www.youtube.com/channel/ParisDeno/live"')
-    }
-}
-
 
 async function findLiveId(channelUrl: string): Promise<string> {
-    const page = await (await fetch(channelUrl)).text();
+  // get channel page
+  const page = await (await fetch(channelUrl)).text();
 
-    const [, liveId] = /i\.ytimg\.com\/vi\/(.*)\//ig.exec(page) ?? [, ''];
+  // find first live id
+  const [, liveId] = /i\.ytimg\.com\/vi\/(.*)\//ig.exec(page) ?? [, ""];
 
-    if (!liveId || new RegExp(`watch\\?v=${liveId}`, "gi").test(page)) {
-        throw new Error("Live ID not found. Is your live set to public?");
-    }
+  // if not found
+  // or if length > 15 (because of mismatch, sometimes we found videos in a playlist)
+  // or if a video link with the same id is found (means that this is a past live)
+  // or if, again, we have a mismatch
+  if (!liveId || liveId.length > 15 || new RegExp(`watch\\?v=${liveId}`, "gi").test(page) || liveId.includes('/hqdefault.jpg')) {
+    throw new Error("Live ID not found. Is your live set to public?");
+  }
 
-    return liveId;
+  return liveId;
 }
