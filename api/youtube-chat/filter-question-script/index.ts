33fc3c400874c59845c5d41200b8b1d01b1004e5
Lilian Saget-Lethias <lilian.sagetlethias@gmail.com>
2020-05-07 00:52:09 +0200

    :bug: Fix assets path resolving

--- api/youtube-chat/filter-question-script/index.ts
+++ api/youtube-chat/filter-question-script/index.ts
@@ -3,24 +3,23 @@ import {
   APIGatewayProxyResult,
   Context,
 } from "https://deno.land/x/lambda/mod.ts";
-import * as path from "https://deno.land/x/std/path/mod.ts";
-
-const __dirname = path.dirname(new URL(import.meta.url).pathname)
 
 export async function handler(
   event: APIGatewayProxyEvent,
   context: Context,
 ): Promise<APIGatewayProxyResult> {
-  const scriptPath = path.resolve(__dirname, "../../script/youtube-chat-filter-question.ts");
+  const scriptPath = "assets/youtube-chat-filter-question.ts";
 
   let [diag, out]: [Deno.DiagnosticItem[] | undefined, string] = [undefined, ''];
     try {
       [diag, out] = await Deno.bundle(scriptPath, void 0, { lib: ['dom', 'esnext']});
     } catch (e) {
-      diag = true as any; // hardcore
+      diag = e as any; // hardcore
     }
   
   if (diag !== null) {
+    console.error('Error during script compilation.');
+    console.error(diag);
     return {
       body: `Error during script compilation.`,
       headers: {
